1d5bb5c88611c1cfb51f2a71092ce4e4e3115482
Grega Krajnc <grega.krajnc@srnjak.com>
2024-04-16 23:03:03 +0200

    Enhance staging repository ID extraction in build workflow
    This commit streamlines the extraction of the staging repository ID in the build workflow. It directly uses the 'extract_staging_repo_id.sh' script within the 'Release to Maven Central' step, therefore eliminating the need for an extra step. This change significantly improves the efficiency and reliability of the release operations to Maven Central.

--- scripts/extract_staging_repo_id.sh
+++ scripts/extract_staging_repo_id.sh
@@ -9,8 +9,14 @@ fi
 # The prefix for staging repository IDs
 PREFIX=$1
 
+# Check if the MVN_SETTINGS_PATH environment variable is set
+if [ -z "$MVN_SETTINGS_PATH" ]; then
+    echo "MVN_SETTINGS_PATH environment variable is not set."
+    exit 1
+fi
+
 # Run the Maven command to list staging repositories and capture the output
-OUTPUT=$(mvn nexus-staging:rc-list)
+OUTPUT=$(mvn nexus-staging:rc-list -s $MVN_SETTINGS_PATH)
 
 # Extract the last repository ID matching the provided prefix
 LAST_REPO_ID=$(echo "$OUTPUT" | grep "$PREFIX" | tail -n1 | awk '{print $2}')
